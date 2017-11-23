2af6652ded04a1a0df12c0e27b882798e732862a
Teemu Halmela <teemuhalmela@gmail.com>
2017-11-23 15:26:43 +0200

    Add sublime user config

---
+++ sublime/add-sublime-user.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+sublime="$HOME/.config/sublime-text-3"
+subtext="$sublime/Packages/User"
+
+if [ ! -d "$sublime" ]; then
+    echo "Please install sublime text 3."
+    exit 1
+fi
+
+if [ -d $subtext ]; then
+    rm -rf $subtext
+fi
+
+ln -s $PWD/User $subtext
