2369467263822573ca683cfee347b06f06a933db
Teemu Halmela <teemuhalmela@gmail.com>
2017-11-23 14:49:18 +0200

    Add bash configs

---
+++ bash/add-bash-configs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+bashrc=$HOME/.bashrc
+lines=(
+"if [ -f ~/.bashrc-mine ]; then"
+"    . ~/.bashrc-mine"
+"fi"
+)
+grep -q -F "${lines[1]}" $bashrc || printf "%s\n" "${lines[@]}" >> $bashrc
+
+mine=$PWD/bashrc-mine
+homemine=$HOME/.bashrc-mine
+ln -sf $mine $homemine
