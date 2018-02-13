bd66113b0c9810041e74bc20ec473084c942ee63
Teemu Halmela <teemuhalmela@gmail.com>
2018-02-13 12:57:45 +0200

    Sublime updates
    - Removed old toggle vintageous (isn't needed anymore because of
    NeoVintageous)
    - Better keybind for console opening
    - Added a new xml indent plugin. This uses xmllint and is a lot of
    faster that Indent X
    - Ignore library and node_modules folders from the index. Putting those
    ignores into the user preferences seemed to be the only way they would
    work

---
+++ sublime/User/Xmlpp.py
@@ -0,0 +1,29 @@
+import sublime
+import sublime_plugin
+import subprocess
+
+# https://gist.github.com/coldnebo/1138554
+# http://www.bergspot.com/blog/2012/05/formatting-xml-in-sublime-text-2-xmllint/
+# https://gist.github.com/jensens/4fc631616f5ef9ac4c6b
+
+class XmlppCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        command = 'xmllint --format --encode utf-8 -'
+
+        if self.view.sel()[0].empty():
+          xmlRegion = sublime.Region(0, self.view.size())
+        else:
+          xmlRegion = self.view.sel()[0]
+
+        p = subprocess.Popen(command, bufsize=-1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
+        result, err = p.communicate(self.view.substr(xmlRegion).encode('utf-8'))
+
+        if err:
+          self.view.set_status('xmlpp', "xmlpp: "+err.decode('utf-8'))
+          sublime.set_timeout(self.clear,10000)
+        else:
+          self.view.replace(edit, xmlRegion, result.decode('utf-8'))
+          sublime.set_timeout(self.clear,0)
+
+    def clear(self):
+        self.view.erase_status('xmlpp')
