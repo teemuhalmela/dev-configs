b022cf267f2c24cfdd8dd01f93a7625a49fc2b26
Teemu Halmela <teemuhalmela@gmail.com>
2018-02-13 13:30:43 +0200

    Sublime text add Json PP
    Use jq to pretty print json.
    Looks like PrettyJson could also support jq, but this is more
    simple.

--- sublime/User/Jsonpp.py
+++ sublime/User/Jsonpp.py
@@ -6,27 +6,27 @@ import subprocess
 # http://www.bergspot.com/blog/2012/05/formatting-xml-in-sublime-text-2-xmllint/
 # https://gist.github.com/jensens/4fc631616f5ef9ac4c6b
 
-# NOTE: This needs xmllint.
-# sudo apt install libxml2-utils
+# NOTE: This needs jq.
+# sudo apt install jq
 
-class XmlppCommand(sublime_plugin.TextCommand):
+class JsonppCommand(sublime_plugin.TextCommand):
     def run(self, edit):
-        command = 'xmllint --format --encode utf-8 -'
+        command = 'jq \'.\''
 
         if self.view.sel()[0].empty():
-          xmlRegion = sublime.Region(0, self.view.size())
+          jsonRegion = sublime.Region(0, self.view.size())
         else:
-          xmlRegion = self.view.sel()[0]
+          jsonRegion = self.view.sel()[0]
 
         p = subprocess.Popen(command, bufsize=-1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE, shell=True)
-        result, err = p.communicate(self.view.substr(xmlRegion).encode('utf-8'))
+        result, err = p.communicate(self.view.substr(jsonRegion).encode('utf-8'))
 
         if err:
-          self.view.set_status('xmlpp', "xmlpp: "+err.decode('utf-8'))
+          self.view.set_status('jsonpp', "jsonpp: "+err.decode('utf-8'))
           sublime.set_timeout(self.clear,10000)
         else:
-          self.view.replace(edit, xmlRegion, result.decode('utf-8'))
+          self.view.replace(edit, jsonRegion, result.decode('utf-8'))
           sublime.set_timeout(self.clear,0)
 
     def clear(self):
-        self.view.erase_status('xmlpp')
+        self.view.erase_status('jsonpp')
