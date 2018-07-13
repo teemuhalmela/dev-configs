d194563ace5e223b195cf0d55d1c35d219095e13
Teemu Halmela <teemuhalmela@gmail.com>
2018-07-13 15:04:27 +0300

    Tmux update
    Remove tmux autostart. Autostart seems to mess up session restore.
    
    History saving also messes up session restore.

--- tmux/add-tmux-conf.sh
+++ tmux/add-tmux-conf.sh
@@ -4,3 +4,8 @@
 # $ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 ln -sf $PWD/tmux.conf $HOME/.tmux.conf
+
+autostartfile=$HOME/.config/systemd/user/tmux.service
+if [[ -f "$autostartfile" ]]; then
+    rm $autostartfile
+fi
