9b7bb6c31efe299748b78dc5c1343ddab61fab90
Teemu Halmela <teemuhalmela@gmail.com>
2019-07-10 08:12:44 +0300

    Backup updates

--- backup/BACKUP-linux.sh
+++ backup/BACKUP-linux.sh
@@ -9,20 +9,23 @@ WORK_DIR=~/TYOT_LINUX
 #DESKTOP_DIR=/cygdrive/c/Users/teemu.halmela/Desktop
 EXCLUDE_FILE=~/TYOT_LINUX/backup/backup_exclude
 
-DIRS_TO_BACKUP=($WORK_DIR)
+readarray includes < "backup_include"
+DIRS_TO_BACKUP=( $WORK_DIR ${includes[@]} )
 
 OLDEST_BACKUP=$BACKUP_DIR/$BACKUP_NAME.3
 MIDDLE2_BACKUP=$BACKUP_DIR/$BACKUP_NAME.2
 MIDDLE1_BACKUP=$BACKUP_DIR/$BACKUP_NAME.1
 LATEST_BACKUP=$BACKUP_DIR/$BACKUP_NAME.0
 
+
+
 ## There are cases when babun hasn't mapped windows dirs properly, so make sure they
 ## are present.
 for i in "${DIRS_TO_BACKUP[@]}"
 do
     DIR_ERROR=false
-    if [ ! -d $i ]; then
-        echo "ERROR: Directory $i not found."
+    if [ ! -d $i ] && [ ! -f $i  ]; then
+        echo "ERROR: Directory/file $i not found."
         DIR_ERROR=true
     fi
 done
@@ -43,10 +46,6 @@ fi
 if [ -d $MIDDLE1_BACKUP ]; then
     mv $MIDDLE1_BACKUP $MIDDLE2_BACKUP
 fi
-# # Saving some space, I don't think hard links work properly on windows.
-# if [ -d $MIDDLE1_BACKUP ]; then
-#     rm -rf $MIDDLE1_BACKUP
-# fi
 
 if [ -d $LATEST_BACKUP ]; then
     mv $LATEST_BACKUP $MIDDLE1_BACKUP
@@ -82,21 +81,21 @@ if [ ! -d $REMOTE_DIR ]; then
     exit 1
 fi
 
-# if [ -f $OLDEST_BACKUP_ZIP ]; then
-#     rm $OLDEST_BACKUP_ZIP
-# fi
+if [ -f $OLDEST_BACKUP_ZIP ]; then
+    rm $OLDEST_BACKUP_ZIP
+fi
 
-# if [ -f $MIDDLE2_BACKUP_ZIP ]; then
-#     mv $MIDDLE2_BACKUP_ZIP $OLDEST_BACKUP_ZIP
-# fi
+if [ -f $MIDDLE2_BACKUP_ZIP ]; then
+    mv $MIDDLE2_BACKUP_ZIP $OLDEST_BACKUP_ZIP
+fi
 
-# if [ -f $MIDDLE1_BACKUP_ZIP ]; then
-#     mv $MIDDLE1_BACKUP_ZIP $MIDDLE2_BACKUP_ZIP
-# fi
+if [ -f $MIDDLE1_BACKUP_ZIP ]; then
+    mv $MIDDLE1_BACKUP_ZIP $MIDDLE2_BACKUP_ZIP
+fi
 
-# if [ -f $LATEST_BACKUP_ZIP ]; then
-#     mv $LATEST_BACKUP_ZIP $MIDDLE1_BACKUP_ZIP
-# fi
+if [ -f $LATEST_BACKUP_ZIP ]; then
+    mv $LATEST_BACKUP_ZIP $MIDDLE1_BACKUP_ZIP
+fi
 
 if [ -f $LATEST_BACKUP_ZIP ]; then
     rm $LATEST_BACKUP_ZIP
@@ -104,71 +103,99 @@ fi
 
 cp $NEW_ZIP $LATEST_BACKUP_ZIP
 
-# WEEKLY_DIR=$REMOTE_DIR/weekly
-# OLDEST_BACKUP_ZIP_W=$WEEKLY_DIR/$BACKUP_NAME.w.3.$ZIP
-# MIDDLE2_BACKUP_ZIP_W=$WEEKLY_DIR/$BACKUP_NAME.w.2.$ZIP
-# MIDDLE1_BACKUP_ZIP_W=$WEEKLY_DIR/$BACKUP_NAME.w.1.$ZIP
-# LATEST_BACKUP_ZIP_W=$WEEKLY_DIR/$BACKUP_NAME.w.0.$ZIP
+WEEKLY_DIR=$REMOTE_DIR/weekly
+OLDEST_BACKUP_ZIP_W=$WEEKLY_DIR/$BACKUP_NAME.w.3.$ZIP
+MIDDLE2_BACKUP_ZIP_W=$WEEKLY_DIR/$BACKUP_NAME.w.2.$ZIP
+MIDDLE1_BACKUP_ZIP_W=$WEEKLY_DIR/$BACKUP_NAME.w.1.$ZIP
+LATEST_BACKUP_ZIP_W=$WEEKLY_DIR/$BACKUP_NAME.w.0.$ZIP
 
-# if [ ! -d $WEEKLY_DIR ]; then
-#     echo "Creating $WEEKLY_DIR dir."
-#     mkdir $WEEKLY_DIR
-# fi
+if [ ! -d $WEEKLY_DIR ]; then
+    echo "Creating $WEEKLY_DIR dir."
+    mkdir $WEEKLY_DIR
+fi
 
-# if [ ! -f $LATEST_BACKUP_ZIP_W ]; then
-#     echo "First weekly backup."
-#     cp $LATEST_BACKUP_ZIP $LATEST_BACKUP_ZIP_W
-# fi
+if [ ! -f $LATEST_BACKUP_ZIP_W ]; then
+    echo "First weekly backup."
+    cp $LATEST_BACKUP_ZIP $LATEST_BACKUP_ZIP_W
+fi
 
-# if [ $(find "$LATEST_BACKUP_ZIP_W" -mtime +5) ]; then
-#     echo "Doing weekly backup."
-#     if [ -f $OLDEST_BACKUP_ZIP_W ]; then
-#         rm $OLDEST_BACKUP_ZIP_W
-#     fi
+if [ $(find "$LATEST_BACKUP_ZIP_W" -mtime +5) ]; then
+    echo "Doing weekly backup."
+    if [ -f $OLDEST_BACKUP_ZIP_W ]; then
+        rm $OLDEST_BACKUP_ZIP_W
+    fi
 
-#     if [ -f $MIDDLE2_BACKUP_ZIP_W ]; then
-#         mv $MIDDLE2_BACKUP_ZIP_W $OLDEST_BACKUP_ZIP_W
-#     fi
+    if [ -f $MIDDLE2_BACKUP_ZIP_W ]; then
+        mv $MIDDLE2_BACKUP_ZIP_W $OLDEST_BACKUP_ZIP_W
+    fi
 
-#     if [ -f $MIDDLE1_BACKUP_ZIP_W ]; then
-#         mv $MIDDLE1_BACKUP_ZIP_W $MIDDLE2_BACKUP_ZIP_W
-#     fi
+    if [ -f $MIDDLE1_BACKUP_ZIP_W ]; then
+        mv $MIDDLE1_BACKUP_ZIP_W $MIDDLE2_BACKUP_ZIP_W
+    fi
 
-#     if [ -f $LATEST_BACKUP_ZIP_W ]; then
-#         mv $LATEST_BACKUP_ZIP_W $MIDDLE1_BACKUP_ZIP_W
-#     fi
+    if [ -f $LATEST_BACKUP_ZIP_W ]; then
+        mv $LATEST_BACKUP_ZIP_W $MIDDLE1_BACKUP_ZIP_W
+    fi
 
-#     cp $LATEST_BACKUP_ZIP $LATEST_BACKUP_ZIP_W
-# fi
+    cp $LATEST_BACKUP_ZIP $LATEST_BACKUP_ZIP_W
+fi
 
-# MONTHLY_DIR=$REMOTE_DIR/monthly
-# MONTHLY_BACKUP_NAME=$MONTHLY_DIR/$BACKUP_NAME.m
-# LATEST_BACKUP_ZIP_M=$MONTHLY_BACKUP_NAME.0.$ZIP
+MONTHLY_DIR=$REMOTE_DIR/monthly
+MONTHLY_BACKUP_NAME=$MONTHLY_DIR/$BACKUP_NAME.m
+LATEST_BACKUP_ZIP_M=$MONTHLY_BACKUP_NAME.0.$ZIP
 
-# if [ ! -d $MONTHLY_DIR ]; then
-#     echo "Creating $MONTHLY_DIR dir."
-#     mkdir $MONTHLY_DIR
-# fi
+if [ ! -d $MONTHLY_DIR ]; then
+    echo "Creating $MONTHLY_DIR dir."
+    mkdir $MONTHLY_DIR
+fi
 
-# if [ ! -f $LATEST_BACKUP_ZIP_M ]; then
-#     echo "First monthly backup."
-#     cp $LATEST_BACKUP_ZIP $LATEST_BACKUP_ZIP_M
-# fi
+if [ ! -f $LATEST_BACKUP_ZIP_M ]; then
+    echo "First monthly backup."
+    cp $LATEST_BACKUP_ZIP $LATEST_BACKUP_ZIP_M
+fi
 
-# if [ $(find "$LATEST_BACKUP_ZIP_M" -mtime +29) ]; then
-#     echo "Doing monthly backup."
-#     if [ -f $MONTHLY_BACKUP_NAME.12.$ZIP ]; then
-#         rm $MONTHLY_BACKUP_NAME.12.$ZIP
-#     fi
-#     for i in $(seq 11 -1 0)
-#     do
-#         FROM=$MONTHLY_BACKUP_NAME.$i.$ZIP
-#         TO=$MONTHLY_BACKUP_NAME.$(($i+1)).$ZIP
-#         if [ -f $FROM ]; then
-#             mv $FROM $TO
-#         fi
-#     done
-#     cp $LATEST_BACKUP_ZIP $LATEST_BACKUP_ZIP_M
-# fi
+if [ $(find "$LATEST_BACKUP_ZIP_M" -mtime +29) ]; then
+    echo "Doing monthly backup."
+    if [ -f $MONTHLY_BACKUP_NAME.12.$ZIP ]; then
+        rm $MONTHLY_BACKUP_NAME.12.$ZIP
+    fi
+    for i in $(seq 11 -1 0)
+    do
+        FROM=$MONTHLY_BACKUP_NAME.$i.$ZIP
+        TO=$MONTHLY_BACKUP_NAME.$(($i+1)).$ZIP
+        if [ -f $FROM ]; then
+            mv $FROM $TO
+        fi
+    done
+    cp $LATEST_BACKUP_ZIP $LATEST_BACKUP_ZIP_M
+fi
+
+YEARLY_DIR=$REMOTE_DIR/yearly
+YEARLY_BACKUP_NAME=$YEARLY_DIR/$BACKUP_NAME.y
+LATEST_BACKUP_ZIP_Y=$YEARLY_BACKUP_NAME.0.$ZIP
+
+if [ ! -d $YEARLY_DIR ]; then
+    echo "Creating $YEARLY_DIR dir."
+    mkdir $YEARLY_DIR
+fi
+
+year=$(date +%Y)
+year_backup="$YEARLY_BACKUP_NAME.$year.$ZIP"
+
+if [ ! -f $LATEST_BACKUP_ZIP_Y ]; then
+    echo "First yearly backup."
+    cp $LATEST_BACKUP_ZIP $LATEST_BACKUP_ZIP_Y
+    cp $LATEST_BACKUP_ZIP $year_backup
+fi
+
+if [ $(find "$LATEST_BACKUP_ZIP_Y" -mtime +365) ]; then
+    echo "Doing yearly backup."
+    if [ -f $year_backup ]; then
+        echo "Backup $year_backup already exists!"
+        exit 1
+    fi
+    cp $LATEST_BACKUP_ZIP $LATEST_BACKUP_ZIP_Y
+    cp $LATEST_BACKUP_ZIP $year_backup
+fi
 
 echo "BACKUP DONE!"
